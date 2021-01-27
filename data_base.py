0ed5c37fffef023d3488ee52bd7629f9fdefd2b8
Tugrul <tugrulpinar@protonmail.com>
2021-01-26 23:34:03 -0500

    First commit.

---
+++ data_base.py
@@ -0,0 +1,10 @@
+class Contacts:
+    def __init__(self):
+        self.secondary_emails = {
+            "JD": "johndoe@email.com", "MJ": "maryjane@email.com"}
+
+
+class SubmissionTypes:
+    def __init__(self):
+        self.choices = {"a": "Application Record", "b": "Reply Memo", "c": "Notice of Discontinuance",
+                        "d": "Notice of Change of Solicitor", "e": "Book of Authorities", "f": "Motion Record", "g": "Letter"}
